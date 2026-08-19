080b431dcff494243327e0ee46163c8e719536ff
vishwakarmakumkum89-prog <vishwakarmakumkum89@gmail.com>
2026-08-19 14:37:59 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,12 @@
+var name ="kumkum";
+console.log(name);
+var id= 123;
+console.log(id);
+var branck= "CSE";
+console.log(branck);
+var department= "Computer Science";
+console.log(department);
+var college= "ABES ENGINEERING COLLEGE";
+console.log(college);
+
+
