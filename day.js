642587217165f6c2160bc8bfabb6d63f57eb3405
vishwakarmakumkum89-prog <vishwakarmakumkum89@gmail.com>
2026-08-19 14:37:09 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,21 @@
+
+// var a=20;
+const app=require('./app.js')
+
+// var b=20;
+// var c=30;
+// console.log(a+b+c);
+var x=20;
+if(x==20){
+    console.log("x is 20")
+}
+else{
+    console.log("x is not 20");
+}
+var x=20;
+const arr=[1,2,3,4,5]
+for (var i=0;i<arr.length;i++){
+    console.log (arr[i]);
+    // console.log (arr[2]);
+}
+console.log(app.xz)
